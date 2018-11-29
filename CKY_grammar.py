804e1eb1ed7a7e425490d493f537fa1e6a0e9f4a
David Rubio Vallejo <David.RUBIOVALLEJO@amadeus.com>
2018-11-29 15:26:47 -0500

    CKY parser and grammar added

---
+++ CKY_grammar.py
@@ -0,0 +1,327 @@
+from tree import *
+from util import *
+
+
+class RHS:
+    def __init__(self, rhs1, rhs2=None):
+        self.rhs1 = rhs1
+        if rhs2 is None:
+            self.isUnary = True
+        else:
+            self.isUnary = False
+            self.rhs2 = rhs2
+
+    def __str__(self):
+        if self.isUnary:
+            return self.rhs1
+        else:
+            return self.rhs1 + " " + self.rhs2
+
+    def __repr__(self):
+        return "'" + str(self) + "'"
+
+    def __eq__(self, other):
+        if self.isUnary != other.isUnary: return False
+        if self.rhs1 != other.rhs1: return False
+        if self.isUnary: return True
+        if self.rhs2 != other.rhs2: return False
+        return True
+
+    def __hash__(self):
+        if self.isUnary:
+            return hash((True, self.rhs1))
+        return hash((False, self.rhs1, self.rhs2))
+
+
+class Rule:
+    def __init__(self, lhs, rhs):
+        self.lhs = lhs
+        self.rhs = rhs
+        self.isUnary = rhs.isUnary
+
+    def __str__(self):
+        return self.lhs + " => " + str(self.rhs)
+
+    def __repr__(self):
+        return "'" + str(self) + "'"
+
+    def __eq__(self, other):
+        if self.lhs != other.lhs: return False
+        return eq(self.rhs, other.rhs)
+
+    def __hash__(self):
+        return hash((self.lhs, self.rhs))
+
+
+class PCFG:
+    def __init__(self, pcfgC={}):
+        self.pcfgC = pcfgC  # the raw cfg itself, pcfgC[lhs][rhs] = Count(lhs -> rhs)
+        self.pcfg = None  # the normalized version of pcfgC, pcfg[lhs][rhs] = p(rhs | lhs)
+        self.pcfgR = None  # a "rotated" version of pcfg, pcfgR[rhs1][rhs2][lhs] = p(rhs1 rhs2 | lhs), where rhs2 is None for unary rules
+
+    def __str__(self):
+        rules = []
+        for lhs in self.pcfgC.keys():
+            for rhs, count in self.pcfgC[lhs].items():
+                rules.append(str(Rule(lhs, rhs)) + '\t| ' + str(count) + '\n')
+        return ''.join(rules)
+
+    def __len__(self):  # number of unique rules
+        count = 0
+        for lhs in self.pcfgC.keys():
+            count += len(self.pcfgC[lhs])
+        return count
+
+    def increase_rule_count(self, rule):
+        self.pcfg = None
+        self.pcfgR = None
+
+        if rule.lhs not in self.pcfgC:
+            self.pcfgC[rule.lhs] = Counter()
+
+        self.pcfgC[rule.lhs][rule.rhs] += 1
+
+    def normalize(self):
+        self.pcfg = {}
+        for lhs in self.pcfgC.keys():
+            # first, copy the elements
+            self.pcfg[lhs] = Counter()
+            for rhs, c in self.pcfgC[lhs].items():
+                self.pcfg[lhs][rhs] = c
+            # now normalize
+            self.pcfg[lhs].normalize()
+        # since pcfg has changed, pcfgR should be reset to None
+        self.pcfgR = None
+
+    def rotate(self):
+        if self.pcfg is None:
+            self.normalize()
+
+        self.pcfgR = {}
+        for lhs in self.pcfg.keys():
+            for rhs, prob in self.pcfg[lhs].items():
+                rhs1 = rhs.rhs1
+                rhs2 = None
+                if not rhs.isUnary:
+                    rhs2 = rhs.rhs2
+
+                # we have a rule of the form "lhs -> rhs1 rhs2"
+                # so set probability of lhs in [rhs1][rhs2] to prob
+
+                if rhs1 not in self.pcfgR:
+                    self.pcfgR[rhs1] = {}
+
+                if rhs2 not in self.pcfgR[rhs1]:
+                    self.pcfgR[rhs1][rhs2] = {}
+
+                self.pcfgR[rhs1][rhs2][lhs] = prob
+
+    def iter_binary_rules_on_rhs(self, rhs1, rhs2):
+        if self.pcfgR is None:
+            self.rotate()
+
+        if rhs1 not in self.pcfgR:
+            return
+
+        if rhs2 not in self.pcfgR[rhs1]:
+            return
+
+        for lhs, ruleLogProb in self.pcfgR[rhs1][rhs2].items():
+            yield (lhs, ruleLogProb)
+
+    def iter_unary_rules_on_rhs(self, rhs):
+        for lhs, ruleLogProb in self.iter_binary_rules_on_rhs(rhs, None):
+            yield (lhs, ruleLogProb)
+
+
+# timeFliesSent = "time flies like an arrow".split()
+
+fluencyFriendPCFG = PCFG({
+    "TOP": {RHS("S"): 1.0},
+
+    "S": {RHS("NP", "VP"): 1.0,
+          RHS("VP"): 1.0,
+          RHS("NP", "VP_PP"): 1.0,
+          RHS("WH2", "VB-ES"): 1.0,
+          RHS("WH1", "NP_VB-ES"): 1.0,
+          RHS("WH1", "NP_VB-HACE"): 1.0,
+          # RHS("VP", "NP_PP"): 1.0,
+          RHS("VP", "PP"): 1.0},
+
+    "NP_VB-ES": {RHS("NP", "VBes"): 1.0},
+    "VB-ES": {RHS("VBes", "NP_PP"): 1.0},
+
+    "NP_VB-HACE": {RHS("NP", "VB-HACE"): 1.0},
+    "VB-HACE": {RHS("VBhace", "PP"): 1.0},
+
+    # "VP_PP": { RHS("VP", "PP")     : 1.0 },
+
+    "NP": {RHS("CNP"): 1.0,
+           RHS("NNP"): 1.0,
+           RHS("CNP", "PP"): 1.0,
+           RHS("UCNP", "PP"): 1.0,
+           RHS("UCNP"): 1.0},
+
+    "CNP": {RHS("DT", "NN"): 1.0,
+            RHS("DT", "AP_NN"): 1.0,
+            RHS("DT", "NPMonth_PP"): 1.0},
+
+    "AP_NN": {RHS("AP", "NN"): 1.0},
+    "NPMonth_PP": {RHS("NPMonth", "PP"): 1.0},
+
+    "UCNP": {RHS("NN"): 1.0},
+
+    "VP": {RHS("VB", "NP"): 1.0,
+           RHS("VB", "NP_PP"): 1.0,
+           RHS("VB"): 1.0,
+           RHS("VB", "S"): 1.0},
+
+    "NP_PP": {RHS("NP", "PP"): 1.0},
+
+    "PP": {RHS("Pin", "NP"): 1.0,
+           RHS("Pin", "NNP"): 1.0,
+           RHS("Pfor", "NPHour"): 1.0,
+           RHS("Pof", "NPYear"): 1.0,
+           RHS("Pof", "NPMonth"): 1.0},
+
+    "NPMonth": {RHS("NB1", "PP"): 1.0},
+    "NPYear": {RHS("NB1", "Mil_NB1"): 1.0},
+
+    "Mil_NB1": {RHS("Mil", "NB1"): 1.0},
+
+    "NPHour": {RHS("DT", "NB1_NB2"): 1.0,
+               RHS("DT", "NB1"): 1.0,
+               RHS("DT", "NB1_CC"): 1.0,
+               RHS("DT", "NB1_ADVMin"): 1.0},
+
+    "NB1_NB2": {RHS("NB1", "NB2"): 1.0},
+
+    "NB1_CC": {RHS("NB1", "CC_NB1"): 1.0,
+               RHS("NB1", "CC_NB2"): 1.0,
+               RHS("NB1", "CC_NB3"): 1.0},
+
+    "CC_NB1": {RHS("CC", "NB1"): 1.0},
+    "CC_NB2": {RHS("CC", "NB2"): 1.0},
+    "CC_NB3": {RHS("CC", "NB3"): 1.0},
+
+    "NB1_ADVMin": {RHS("NB1", "ADVMin_NB1"): 1.0,
+                   RHS("NB1", "ADVMin_NB2"): 1.0,
+                   RHS("NB1", "ADVMin_NB3"): 1.0, },
+
+    "ADVMin_NB1": {RHS("ADVMin", "NB1"): 1.0},
+    "ADVMin_NB2": {RHS("ADVMin", "NB2"): 1.0},
+    "ADVMin_NB3": {RHS("ADVMin", "NB3"): 1.0},
+
+    "NB1": {RHS("Num"): 1.0},
+    "NB2": {RHS("Num", "CC_Num"): 1.0},
+    "NB3": {RHS("Num3"): 1.0},
+
+    "CC_Num": {RHS("CC", "Num"): 1.0},
+
+    # Start of lexicon
+    "WH1": {RHS('que'): 1.0},
+    "WH2": {RHS('cual'): 1.0},
+
+    # cuanto WH num:sg
+    # cuando WH num:sg
+    # como
+    # quien WH num:sg
+    # quienes WH num:pl
+
+    "VB": {RHS('muestrame'): 1.0,
+           RHS('enseñame'): 1.0,
+           RHS('dime'): 1.0,
+           RHS('enciende'): 1.0,
+           RHS('apaga'): 1.0,
+           RHS('recomiendame'): 1.0,
+           RHS('escribe'): 1.0,
+           RHS('pon'): 1.0,
+           RHS('crea'): 1.0},
+
+    "VBes": {RHS('es'): 1.0},
+    "VBhace": {RHS('hace'): 1.0},
+
+    "NN": {RHS('alarma'): 1.0,
+           RHS('nota'): 1.0,
+           RHS('tiempo'): 1.0,
+           RHS('hora'): 1.0,
+           RHS('luz'): 1.0,
+           RHS('luces'): 1.0,
+           RHS('restaurantes'): 1.0},
+
+    "DT": {RHS('las'): 1.0,
+           RHS('la'): 1.0,
+           RHS('los'): 1.0,
+           RHS('el'): 1.0,
+           RHS('una'): 1.0},
+
+    "Num": {RHS('uno'): 1.0,
+            RHS('una'): 1.0,
+            RHS('dos'): 1.0,
+            RHS('tres'): 1.0,
+            RHS('cuatro'): 1.0,
+            RHS('cinco'): 1.0,
+            RHS('seis'): 1.0,
+            RHS('siete'): 1.0,
+            RHS('ocho'): 1.0,
+            RHS('nueve'): 1.0,
+            RHS('diez'): 1.0,
+            RHS('once'): 1.0,
+            RHS('doce'): 1.0,
+            RHS('trece'): 1.0,
+            RHS('catorce'): 1.0,
+            RHS('quince'): 1.0,
+            RHS('dieciseis'): 1.0,
+            RHS('diecisiete'): 1.0,
+            RHS('dieciocho'): 1.0,
+            RHS('diecinueve'): 1.0,
+            RHS('veinte'): 1.0,
+            RHS('veintiuno'): 1.0,
+            RHS('veintidos'): 1.0,
+            RHS('veintitres'): 1.0,
+            RHS('veinticuatro'): 1.0,
+            RHS('veinticinco'): 1.0,
+            RHS('veintiseis'): 1.0,
+            RHS('veintisiete'): 1.0,
+            RHS('veintiocho'): 1.0,
+            RHS('veintinueve'): 1.0,
+            RHS('treinta'): 1.0,
+            RHS('cuarenta'): 1.0,
+            RHS('cincuenta'): 1.0,
+            RHS('sesenta'): 1.0},
+
+    "Mil": {RHS('mil'): 1.0},
+    "CC": {RHS('y'): 1.0},
+
+    "Num3": {RHS('media'): 1.0,
+             RHS('cuarto'): 1.0},
+
+    "Pin": {RHS('en'): 1.0},
+
+    "Pfor": {RHS('para'): 1.0,
+             RHS('a'): 1.0},
+
+    "Pof": {RHS('de'): 1.0},
+
+    "ADVMin": {RHS('menos'): 1.0},
+    "ADV": {RHS('mas'): 1.0},
+
+    "NMonth": {RHS('enero'): 1.0,
+               RHS('febrero'): 1.0,
+               RHS('marzo'): 1.0,
+               RHS('abril'): 1.0,
+               RHS('mayo'): 1.0,
+               RHS('junio'): 1.0,
+               RHS('julio'): 1.0,
+               RHS('agosto'): 1.0,
+               RHS('septiembre'): 1.0,
+               RHS('octubre'): 1.0,
+               RHS('noviembre'): 1.0,
+               RHS('diciembre'): 1.0},
+
+    "NNP": {RHS('waltham'): 1.0,
+            RHS('boston'): 1.0,
+            RHS('cambridge'): 1.0,
+            RHS('lexington'): 1.0}
+})
+
---
+++ CKY_parser.py
@@ -0,0 +1,216 @@
+
+from math import *
+from grammar import *
+
+class Item:
+    def __init__(self, i, j, label, logProb=0, backPtrLeft=None, backPtrRight=None):
+        self.startPos = i
+        self.endPos = j
+        self.label = label
+        self.logProb = logProb
+        self.backPtrLeft = backPtrLeft
+        self.backPtrRight = backPtrRight
+
+    def get_tree(self):
+        if self.backPtrLeft is None:    # this is a terminal rule
+            return self.label
+        elif self.backPtrRight is None: # this is a unary production
+            return Tree(self.label, [self.backPtrLeft.get_tree()])
+        else:                           # this is a binary production
+            return Tree(self.label, [self.backPtrLeft.get_tree(), self.backPtrRight.get_tree()])
+
+    def __str__(self):
+        return self.label + "[" + str(self.startPos) + ':' + str(self.endPos) + "]"
+
+
+class Chart:
+    def __init__(self, pruningPercent=None):
+        self.chart = {}
+        self.greatestLogProb = {}
+        self.pruningPercent = pruningPercent
+
+    def iter_cell(self, i, j):
+        if i not in self.chart: return
+        if j not in self.chart[i]: return
+        for item in self.chart[i][j].values():
+            yield item
+
+    def best_in_cell(self, i, j, desiredLabel=None):
+        bestItem = None
+        bestLogProb = float('-inf')
+        for item in self.iter_cell(i, j):
+            if desiredLabel is not None and desiredLabel != item.label: continue
+            if item.logProb > bestLogProb:
+                bestItem    = item
+                bestLogProb = item.logProb
+        return bestItem
+
+    def prune_cell(self, i, j):
+        # kill anything worse than pruningPercent times the probability of the best thing
+        if self.pruningPercent is None: return
+        
+        bestItem = self.best_in_cell(i, j)
+        if bestItem is None: return
+
+        threshold = log(self.pruningPercent) + bestItem.logProb
+        toRemove = []
+        for item in self.iter_cell(i,j):
+            if item.logProb < threshold:
+                toRemove.append(item.label)
+
+        for label in toRemove:
+            self.remove(i, j, label)
+
+    def remove(self, i, j, label):
+        if i not in self.chart: return
+        if j not in self.chart[i]: return
+        if label not in self.chart[i][j]: return
+        del self.chart[i][j][label]
+
+    def add(self, item):
+        i = item.startPos
+        j = item.endPos
+        if i not in self.chart:
+            self.chart[i] = {}
+            self.greatestLogProb[i] = {}
+        if j not in self.chart[i]:
+            self.chart[i][j] = {}
+            self.greatestLogProb[i][j] = float('-inf')
+            
+        # only add if we don't already have a better version
+        shouldAdd = True
+        if item.label in self.chart[i][j]:
+            # easy case: if we're not better, we shouldn't add
+            if item.logProb <= self.chart[i][j][item.label].logProb:
+                shouldAdd = False
+
+            # pruning case: if we're not better than the best*pruningPercent, we shouldn't add
+            if self.pruningPercent is not None and item.logProb < log(self.pruningPercent) + self.greatestLogProb[i][j]:
+                shouldAdd = False
+
+        if shouldAdd:
+            self.chart[i][j][item.label] = item
+            if item.logProb > self.greatestLogProb[i][j]:
+                self.greatestLogProb[i][j] = item.logProb
+
+        return shouldAdd
+
+def cky(pcfg, sent, pruningPercent=None):
+    N = len(sent)
+
+    # chart[i][j] holds all ways to parse the string
+    # sent[i]... sent[j] inclusive
+    chart = Chart(pruningPercent)
+
+    # base case: fill in chart for each position's word
+    for i in range(N):
+        item = Item(i, i+1, sent[i], logProb=0)   # logProb=0 means probability=1
+        chart.add(item)
+
+    # base case: unary rules -- need to be sure we don't loop!
+    for i in range(N):
+        while True:
+            toAdd = []
+            for item in chart.iter_cell(i, i+1):
+                for lhs,ruleProb in pcfg.iter_unary_rules_on_rhs(item.label):
+                    newItem = Item(i, i+1, lhs, item.logProb + log(ruleProb), item)
+                    # print("UNARY ", item.get_tree())
+                    toAdd.append(newItem)
+
+            anyAdded = False
+            for item in toAdd:
+                if chart.add(item):
+                    anyAdded = True
+                
+            if not anyAdded:
+                break
+
+        chart.prune_cell(i, i+1)
+
+    # recursive case
+    for spanSize in range(2,N+1):
+
+        for i in range(N-spanSize+1):
+            toAdd = []
+            k = i + spanSize
+            for j in range(i+1, k):
+                # consider creating a cell spanning i -> k, with a split
+                # point at k.  in other words, we want to merge [i,j] with
+                # [j+1,k]
+                for rhs1 in chart.iter_cell(i,j):
+                    for rhs2 in chart.iter_cell(j, k):
+                        for lhs,ruleProb in pcfg.iter_binary_rules_on_rhs(rhs1.label, rhs2.label):
+                            # make a new item
+                            item = Item(i, k, lhs, rhs1.logProb + rhs2.logProb + log(ruleProb), rhs1, rhs2)
+                            # print("BINARY "+str(i)+" "+str(j)+" "+str(k),item.get_tree())
+                            chart.add(item)
+                            # toAdd.append(item)
+
+            # try unary rules
+            for rhs in chart.iter_cell(i,k):
+                for lhs,ruleProb in pcfg.iter_unary_rules_on_rhs(rhs.label):
+                    newItem = Item(i, k, lhs, rhs.logProb + log(ruleProb), rhs)
+                    toAdd.append(newItem)
+                    # print("UNARY " , newItem.label)#newItem.get_tree())
+                    # chart.add(newItem)
+            for item in toAdd:
+                chart.add(item)
+
+            # prune the cell
+            chart.prune_cell(i, k)
+    
+    return chart
+
+def parse(pcfg, sent, pruningPercent=None):
+    N = len(sent)
+    chart = cky(pcfg, sent, pruningPercent)
+    top = chart.best_in_cell(0, N, 'TOP')
+    if top is None:
+        return None
+    return top.get_tree()
+
+def evaluateParser(pcfg, filename, pruningPercent=None, horizSize=None, verticSize=1, runFancyCode=False):
+    averageAcc = 0.
+    totalCount = 0.
+    for tree in iterateTreebank(filename, horizSize=horizSize, verticSize=verticSize, runFancyCode=runFancyCode):
+        sent = tree.preterminals()
+        sys.__stderr__.write('.')
+        res  = parse(pcfg, sent, pruningPercent)
+        averageAcc += evaluate(debinarizeTree(tree), debinarizeTree(res))
+        totalCount += 1.0
+    sys.__stderr__.write('\n')
+    return averageAcc/totalCount
+
+
+def reinsertWords(tree, sent):
+    def reinsertWords_rec(tree, i):
+        for j in range(len(tree)):
+            if isinstance(tree[j], Tree):
+                i,tree[j] = reinsertWords_rec(tree[j], i)
+            else:
+                tree[j] = Tree(tree[j], [sent[i]])
+                i = i + 1
+        return i,tree
+    j,tree = reinsertWords_rec(tree, 0)
+    return tree
+
+def runParserOnTest(pcfg, testFilename, outputFilename, pruningPercent=None, horizSize=None, verticSize=1, runFancyCode=False):
+    h = open(outputFilename, 'w')
+    for (sent,tags) in iterateTaggedSentences(testFilename):
+        sys.__stderr__.write('.')
+        res  = parse(pcfg, tags, pruningPercent)
+        if res is None:
+            h.write('None\n')
+        else:
+            h.write(repr(reinsertWords(debinarizeTree(res), sent)))
+            h.write('\n')
+    sys.__stderr__.write('\n')
+        
+
+if __name__ == "__main__":
+
+    testSent = "crea una alarma para las seis menos cinco".split()
+
+    print(testSent)
+    print()
+    print(parse(fluencyFriendPCFG, testSent))
