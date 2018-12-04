6d0e63dd97372930e93205f343d991e31f544308
micaelakaplan <32578813+micaelakaplan@users.noreply.github.com>
2018-12-04 10:11:30 -0500

    alexa basics for launch request

--- webhook_fulfillment.py
+++ webhook_fulfillment.py
@@ -211,7 +211,8 @@ def manage_request():
     try:
         req = request.get_json(silent=True, force=True)
         if 'request' in req.keys():
-            if req.get("request").get("type") is "LaunchRequest":
+            response = "correct json"
+            if req.get('request').get('type') is 'LaunchRequest':
                 response = "Hello, welcome to Fluency Friend! If you ask me to do something in English, I can teach you to say it in Spanish. Ask me in Spanish and I can correct you!"
                 ssml = "<speak> Hello, welcome to Fluency Friend! If you ask me to do something in English, I can teach you to say it in Spanish. Ask me in Spanish and I can correct you! </speak>"
 
