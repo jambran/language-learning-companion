e9554e2ab5325c98193f09ba81424cbfd4705f98
micaelakaplan <32578813+micaelakaplan@users.noreply.github.com>
2018-12-15 16:00:34 -0500

    debugging-- elif

--- webhook_fulfillment.py
+++ webhook_fulfillment.py
@@ -293,12 +293,8 @@ def get_english_intent_ssml(intent, req):
         ssml += "<speak> You can say: <lang xml:lang ='es-ES'>Pon la alarma para <say-as interpret-as = 'cardinal'>"+time+"</say-as></lang></speak>"
     elif intent == 'Calendar':
         # GET SLOT INFO FOR DATE
-        date = req.get('request').get('intent').get('slots').get('dateslot').get('value')
-        ssml += "<speak> You could say: <lang xml:lang = 'es-ES'>Crea una nota para <say-as interpret-as = 'date' format = 'md'>"+date+"</say-as></lang></speak>"
-    elif intent == 'Calendar':
-        # GET SLOT INFO FOR DATE
-        date = req.get('request').get('intent').get('slots').get('dateslot').get('value')
-        ssml += "<speak> You could say: <lang xml:lang = 'es-ES'>Crea una nota para <say-as interpret-as = 'date' format = 'md'>" + date + "</say-as></lang></speak"
+        time = req.get('request').get('intent').get('slots').get('timeslot').get('value')
+        ssml += "<speak> You can say: <lang xml:lang ='es-ES'>Pon una nota <say-as interpret-as = 'date'>"+time+"</say-as></lang></speak>">"
     elif intent == 'Weather':
         # GET SLOT INFO FOR CITY
         city = req.get('request').get('intent').get('slots').get('city').get('value')
